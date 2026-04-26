e52c5baaa5466746d7f3a4b8e3cf3def910eedf7
cudiHAHA <04petrovaleksandr@gmail.com>
2026-04-26 17:35:57 +0300

    feat: return greeting from function

--- main.go
+++ main.go
@@ -1,17 +1,15 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
+
 	name := "Александр"
-	repo := "go-basics"
-	language := "GO"
-	goal := "стать GO-разработчиком"
-	fmt.Println("Меня зовут", name)
-	fmt.Println("Мой репозиторий:", repo)
-	fmt.Println("Я учусь", language)
-	fmt.Println("Сегодня я настроил среду разработки и написал свою первую программу на", language)
-	fmt.Println("Моя цель:", goal)
+
+	greeting := makeGreeting(name)
+	fmt.Println(greeting)
+}
+
+func makeGreeting(name string) string {
+	return "Привет, меня зовут " + name
 }
